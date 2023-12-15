2f032a76fca6eeffee005ef37a6c779bddc31b51
shinyingrid <shinyingrid88@gmail.com>
2023-12-15 20:53:19 +0530

    New change in Eclipse

--- src/test/java/GITBASH_sample/SampleGitBash1.java
+++ src/test/java/GITBASH_sample/SampleGitBash1.java
@@ -5,8 +5,7 @@ public class SampleGitBash1 {
 	public static void main(String[] args) 
 	{
 		System.out.println("Gitbash Sample Program");
-		// changes made in GITHUB repo
-		//changes made in Eclipse project
+		//Newly made change in Eclipse
 
 	}
 
